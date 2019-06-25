f0abe1845fb045ef43c1fe230fcb20e017aa5751
misocho <misochobrian@gmail.com>
2019-06-25 11:09:05 +0300

    Questioner React App setup - Add travis and coveralls integration

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div data-test='component-app'>Thi sis the main app</div>
+    );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index_bundle.js",
+        publicPath: "/"
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+            }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ]
+};
